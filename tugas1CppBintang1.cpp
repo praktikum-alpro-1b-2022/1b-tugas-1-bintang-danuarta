05e05a705939da4d9c3958a9d6b00bf132dc1c0f
BintangDanuarta <60837533+BintangDanuarta@users.noreply.github.com>
2022-10-06 14:25:34 +0700

    Upload tugas pertama Alpro nomor 1

---
+++ tugas1CppBintang1.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // input, output, stream
+using namespace std;
+
+//tugas nomor 1
+int main(){
+    cout << "NPM: 2210631170014\n";
+    cout << "Nama: Bintang Danuarta\n";
+    cout << "Umur: 18 Tahun\n";
+    cout << "Kelas: 1B Informatika\n";
+    cout << "Hobi: Ngevent wibu\n";
+    cout << "Cita-cita: UI/UX Designer\n";
+
+    return 0;
+}
